3889ca29103de4a2d50f86d27968042b0f247764
Yevhen <yevhen.yakymenko.fed@gmail.com>
2022-11-23 00:53:21 +0200

    styled AuthNav

--- src/components/AuthNavList/AuthNavList.jsx
+++ src/components/AuthNavList/AuthNavList.jsx
@@ -1,12 +1,14 @@
-import { NavLink } from 'react-router-dom';
-
-import { BtnWrapper } from './AuthNavList.styled';
+import {
+  BtnWrapper,
+  SyledLink,
+  StyledLinkSecondary,
+} from './AuthNavList.styled';
 
 const AuthNavList = () => {
   return (
     <BtnWrapper>
-      <NavLink to={'signup'}>Sign Up</NavLink>
-      <NavLink to={'login'}>Log In</NavLink>
+      <SyledLink to={'login'}>Log In</SyledLink>
+      <StyledLinkSecondary to={'signup'}>Sign Up</StyledLinkSecondary>
     </BtnWrapper>
   );
 };
--- src/components/AuthNavList/AuthNavList.styled.js
+++ src/components/AuthNavList/AuthNavList.styled.js
@@ -1,5 +1,39 @@
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const BtnWrapper = styled.nav`
   flex-grow: 1;
+  display: flex;
+  justify-content: end;
+  gap: ${p => p.theme.space[3]}px;
+`;
+
+export const SyledLink = styled(NavLink)`
+  padding: ${p => p.theme.space[2]}px ${p => p.theme.space[3]}px;
+  font-weight: ${p => p.theme.fontWeights.bold};
+  color: ${p => p.theme.colors.btnText};
+  background: ${p => p.theme.colors.btnBackground};
+  border-radius: ${p => p.theme.space[2]}px;
+  border: 1px solid ${p => p.theme.colors.border};
+  cursor: pointer;
+  transition: color 100ms linear, background 100ms linear;
+
+  &:hover {
+    color: ${p => p.theme.colors.white};
+    background: ${p => p.theme.colors.btnAccent};
+  }
+`;
+
+export const StyledLinkSecondary = styled(NavLink)`
+  padding: ${p => p.theme.space[2]}px ${p => p.theme.space[3]}px;
+  font-weight: ${p => p.theme.fontWeights.medium};
+  color: ${p => p.theme.colors.white};
+  background: ${p => p.theme.colors.btnAccent};
+  border-radius: ${p => p.theme.space[2]}px;
+  cursor: pointer;
+  transition: opacity 100ms linear;
+
+  &:hover {
+    opacity: 0.8;
+  }
 `;
